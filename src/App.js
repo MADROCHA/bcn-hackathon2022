337b7d3f7c31a601158f05368466932eb33f80fb
MADROCHA <madlarocha@gmail.com>
2022-06-18 21:28:00 +0200

    App.js_updated_hook_useffect//style_changes

--- src/App.js
+++ src/App.js
@@ -24,41 +24,36 @@ function App() {
 			supportDrives: true,
 			multiselect: true,
 
-			callbackFunction: (data) => {
-				if (data.action === "cancel") {
-					console.log("User clicked cancel/close button")
-				}
-				console.log(data)
-			},
-		})
-		
-		useEffect( () => {
-			if (data){
-				data.docs.map((i) => console.log(i))
-			}
-		},[data])
+
+		});
 	}
+useEffect( () => {
+	if (data){
+		data.docs.map((i) => console.log(i));
+	}
+	},[data])
 
 	return (
 		<div className="App">
-			<>
-				<div className = 'dropZone-container'>
-					<div className = 'green-container'>
-					</div>
-					<DropZoneComponent/>
-					<Button 
-						text = 'Subir tus archivos' /> 
-				</div>
+		<>
+			<div className = 'dropZone-container'>
+			<div className = 'green-container'>
+			</div>
+			<DropZoneComponent></DropZoneComponent>
+			<Button 
+				text = 'Subir tus archivos' /> 
+			</div>
 
-				<div className = 'login-welcome-container'>
-					<h2>Bienvenido a DDrop</h2>
-					<h6>Para subir tus archivos de forma simple a drive, puedes hacer LogIn a través de Google.</h6>
-					<hr className = 'line' />
-					<LoginButton
-						className = 'login-container'
-						text = 'Login con Google'
-						onClick = {()=>handleOpenPicker()} />
-				</div>
+			<div className = 'login-welcome-container'>
+			<h2>Bienvenido a DDrop</h2>
+			<h6>Para subir tus archivos de forma simple a drive, puedes hacer LogIn a través de Google.</h6>
+			<hr className = 'line' />
+			<LoginButton
+				className = 'login-container'
+				text = 'Login con Google'
+				onClick = {()=>handleOpenPicker()} 
+				type = "button"/>
+		</div>
 			</>
 
 		</div>
