c0b12b71128476acb060f23d7ec57a96e5fd7bc3
MADROCHA <madlarocha@gmail.com>
2022-06-19 13:38:29 +0200

    Api_Fixed//Dropzone.jsx->Function_drag_WIP

--- src/components/DropZoneComponent.jsx
+++ src/components/DropZoneComponent.jsx
@@ -1,26 +1,40 @@
-import React, { useCallback, useState } from "react"
 import "../stylesheets/DropZoneComponent.css"
 import dropbg from "../images/dropbg.png"
+import React, { useCallback, useState } from "react"
 import Dropzone, {useDropzone} from "react-dropzone"
 
 function DropZoneComponent(){
-
-/* 	const [ selectedFiles, setSelectedFiles] = useState([]);
+/* 
+	const [ selectedFiles, setSelectedFiles] = useState([]);
+	
 	const onGrab = useCallback()(acceptedFiles => {
-        
 		setSelectedFiles(acceptedFiles.map(file => 
             Object.assign(file,{
                 preview: URL.createObjectURL(file)
             })
             ));
-        }, []); */
+        }, []);
+
+	const { getRootProps, getInputProps } = useDropzone({onGrab})
+
+	const selected_files = selectedFiles?.map(file => (
+		<div>
+			<img src={file.preview} style={{width:'100px', heigth: '100px'}} alt='' />
+		</div>
+	))
+ */
 
 
 
 
 
+	return( 
+	
+<>
+{/* 	<div {...getRootProps()}/>
+	<input {...getInputProps()} /> */}
+	
 
-	return(
 		<div className = 'dropzone-container' >
 			<p> ARRASTRA TUS ARCHIVOS AQUÍ </p>
 			<img
@@ -29,7 +43,9 @@ function DropZoneComponent(){
 				alt = 'background for the dropzone container'/>
 
 		</div>
+</>
 	)
+
 }
 
 export default DropZoneComponent
